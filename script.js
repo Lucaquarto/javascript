1d08ead4e5dece0d09b26bef07beaa8f3bab3fcb
Alphabb07 <162568674+Alphabb07@users.noreply.github.com>
2024-09-23 11:55:55 +0200

    ciao
    creato index

---
+++ script.js
@@ -0,0 +1,28 @@
+let ans1 = prompt("1.Quanto sei alto")
+ans1 = <ans1 class="tolowercase"></ans1>
+ans1 = ans1.trim()
+if(ans1 == giuseppe)
+
+
+
+
+{
+    let ans2 = prompt("2.Quanti anni hai")
+    let ans3 = prompt ("qual'e il capuologo dell'italia")
+    let ans4 = prompt ("chi ha segnato nella famosa partita bari cagliari il gol del vantaggio")
+    let ans5 = prompt ("quanti anni ha fedez")
+    let ans6 = prompt ("quando inizia l'estate")
+    let ans7 = prompt ("in che anno Cristiano Ronaldo ando' alla juventus")
+
+    if (ans1 === null)
+
+}
+
+
+console.log("benvenuto"+ firstname)
+
+.trim()
+.tolowercase()
+
+alert("Punteggio: " + punteggio)
+
